63b12b7ca9c80ba9be8941623a2a48d8e2dd3b97
caster99yzw <15675512306@163.com>
2017-08-06 17:47:06 +0800

    Initialize the project.

---
+++ Y3DCommon/Y3DPI.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Y3D
+{
+	const float Y3D_PI				= 3.1415926535f;
+	const float Y3D_PI_DB			= 6.2831853072f;
+	const float Y3D_PI_HF		= 1.5707963268f;
+	const float Y3D_PI_QT		 = 0.7853981634f;
+
+
+}
---
+++ Y3DCommon/Y3DVector.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Y3D
+{
+	class Vector3
+	{
+
+	};
+}
